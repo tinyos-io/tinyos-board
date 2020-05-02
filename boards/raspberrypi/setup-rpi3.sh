b595b5094a0c5d357fba7c2c9589fb8a07d74c5a
abdullatifmouhamadi <abdullatif.mouhamadi@gmail.com>
2020-05-02 18:11:48 +0200

    updating rm

--- boards/raspberrypi/setup-rpi3.sh
+++ boards/raspberrypi/setup-rpi3.sh
@@ -3,5 +3,6 @@
 
 
 
- wget http://os.archlinuxarm.org/os/ArchLinuxARM-rpi-2-latest.tar.gz
+ #wget http://os.archlinuxarm.org/os/ArchLinuxARM-rpi-2-latest.tar.gz
+ wget http://os.archlinuxarm.org/os/ArchLinuxARM-rpi-3-latest.tar.gz
 
